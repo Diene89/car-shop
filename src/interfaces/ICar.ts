da5d90cd7947f8260dc280fd2dcbc7490b8f1956
Diene89 <diene_89@hotmail.com>
2022-09-14 15:48:22 -0300

    feat: cria interface ICar

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const CarZodSchema = VehicleZodSchema.extend({
+  doorsQty: z.number().int().gte(2).lte(4),
+  seatsQty: z.number().int().gte(2).lte(7),
+});
+
+type ICar = z.infer<typeof CarZodSchema>;
+
+export { CarZodSchema, ICar };
