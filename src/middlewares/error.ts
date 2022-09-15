bdfa3c21790ec1a0dde502b2bc3ff2694084619d
Diene89 <diene_89@hotmail.com>
2022-09-15 00:29:12 -0300

    retira console.log

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,8 +2,6 @@ import { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
 
 const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
-  console.log(err, 'meu erroooooo');
-
   if (err instanceof ZodError) { return res.status(400).json(err.issues); }
 };
 
